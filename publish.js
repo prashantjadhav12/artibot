50d0fa6c82819b274c385ff75fbf231dbc563308
Prashant Jadhav <jadhav.prashant12@gmail.com>
2017-10-29 17:10:46 +0530

    adding source

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../artibot.zip');
+var kuduApi = 'https://artibot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$artibot';
+var password = 'F83MDM0ldDnAKoHdAm2sguNmsukfa51DwhpqrjbPq0YN7mbaQu8G4MGEQR0d';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('artibot publish');
+  } else {
+    console.error('failed to publish artibot', err);
+  }
+});
